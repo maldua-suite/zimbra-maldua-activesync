caf76e13ba449e6efe274899114d5f5ce4e92869
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2025-09-17 19:52:14 +0200

    Admin Extension: ZetaActiveSyncExtension.

---
+++ extension/src/com/btactic/activesync/service/message/ZetaActiveSyncAuthRequest.java
@@ -0,0 +1,30 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Maldua Zimbra ActiveSync Extension
+ * Copyright (C) 2025 BTACTIC, S.C.C.L.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.btactic.activesync.service.message;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import com.zimbra.soap.account.message.AuthRequest;
+import com.btactic.activesync.service.ZetaActiveSyncConstants;
+
+@XmlRootElement(name=ZetaActiveSyncConstants.E_ZETA_ACTIVESYNC_AUTH_REQUEST)
+@XmlType(propOrder = {})
+public class ZetaActiveSyncAuthRequest extends AuthRequest {
+
+}
