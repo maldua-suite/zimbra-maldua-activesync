375ca307451c56df164521e8ed98801f8db2d53c
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2025-09-19 11:21:51 +0200

    ZetaActiveSyncAuth: Extensive Auth debug. (11)

--- extension/src/com/btactic/activesync/service/ZetaActiveSyncAuth.java
+++ extension/src/com/btactic/activesync/service/ZetaActiveSyncAuth.java
@@ -14,60 +14,100 @@
  */
 package com.btactic.activesync.service;
 
-import com.zimbra.common.service.ServiceException;
-
-import com.zimbra.common.soap.AccountConstants;
-import com.zimbra.common.soap.Element;
-
-import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.service.account.Auth;
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.soap.Element;
+import com.zimbra.common.soap.AccountConstants;
 import com.zimbra.soap.SoapEngine;
-import com.zimbra.soap.ZimbraSoapContext;
 
 import java.util.Map;
 
-public class ZetaActiveSyncAuth {
+/**
+ * Wraps Auth to inject port and protocol overrides before delegating.
+ */
+public class ZetaActiveSyncAuth extends Auth {
 
+    @Override
     public Element handle(Element request, Map<String, Object> context) throws ServiceException {
-        // Construct a "full" AuthRequest with all possible elements
-        Element authRequest = new Element.XMLElement("AuthRequest");
+        // override protocol and port before delegating
+        context.put("proto", "sync"); // or Protocol.sync if available
+        context.put(SoapEngine.REQUEST_PORT, context.getOrDefault(SoapEngine.REQUEST_PORT, 7073));
 
-        // Copy account and 'by' attribute
-        Element accountEl = request.getOptionalElement(AccountConstants.E_ACCOUNT);
-        if (accountEl != null) {
-            authRequest.addElement(AccountConstants.E_ACCOUNT)
-                       .addAttribute(AccountConstants.A_BY, accountEl.getAttribute(AccountConstants.A_BY, "name"))
-                       .setText(accountEl.getText());
-        }
+        // Create a new request element to replicate the original for Auth
+        Element newRequest = Element.create(request.getQName());
 
-        // Copy password / recoveryCode / preauth / authToken / jwtToken / deviceId / 2FA
-        copyOptional(request, authRequest, AccountConstants.E_PASSWORD);
-        copyOptional(request, authRequest, AccountConstants.E_RECOVERY_CODE);
-        copyOptional(request, authRequest, AccountConstants.E_PREAUTH);
-        copyOptional(request, authRequest, AccountConstants.E_AUTH_TOKEN);
-        copyOptional(request, authRequest, "jwtToken");
-        copyOptional(request, authRequest, AccountConstants.E_DEVICE_ID);
-        copyOptional(request, authRequest, AccountConstants.E_TWO_FACTOR_CODE);
-        copyOptional(request, authRequest, AccountConstants.A_GENERATE_DEVICE_ID);
-        copyOptional(request, authRequest, AccountConstants.A_TRUSTED_DEVICE);
-
-        // Copy prefs / attrs / requestedSkin
-        copyOptional(request, authRequest, AccountConstants.E_PREFS);
-        copyOptional(request, authRequest, AccountConstants.E_ATTRS);
-        copyOptional(request, authRequest, AccountConstants.E_REQUESTED_SKIN);
-
-        // Delegate to actual Auth handler
-        return new Auth().handle(authRequest, context);
-    }
-
-    private void copyOptional(Element source, Element target, String elementName) throws ServiceException {
-        Element child = source.getOptionalElement(elementName);
-        if (child != null) {
-            Element newChild = target.addElement(elementName).setText(child.getText());
-            for (String attrName : child.getAttributeNames()) {
-                newChild.addAttribute(attrName, child.getAttribute(attrName));
+        // Copy E_ACCOUNT if present
+        Element acctEl = request.getOptionalElement(AccountConstants.E_ACCOUNT);
+        if (acctEl != null) {
+            Element newAcctEl = newRequest.addElement(AccountConstants.E_ACCOUNT)
+                                         .setText(acctEl.getText());
+            String byAttr = acctEl.getAttribute(AccountConstants.A_BY, null);
+            if (byAttr != null) {
+                newAcctEl.addAttribute(AccountConstants.A_BY, byAttr);
+            }
+            Element virtualHostEl = acctEl.getOptionalElement(AccountConstants.E_VIRTUAL_HOST);
+            if (virtualHostEl != null) {
+                newAcctEl.addElement(AccountConstants.E_VIRTUAL_HOST)
+                         .setText(virtualHostEl.getText());
             }
         }
-    }
 
+        // Copy password
+        String password = request.getAttribute(AccountConstants.E_PASSWORD, null);
+        if (password != null) {
+            newRequest.addAttribute(AccountConstants.E_PASSWORD, password);
+        }
+
+        // Copy recovery code if present
+        String recoveryCode = request.getAttribute(AccountConstants.E_RECOVERY_CODE, null);
+        if (recoveryCode != null) {
+            newRequest.addAttribute(AccountConstants.E_RECOVERY_CODE, recoveryCode);
+        }
+
+        // Copy generateDeviceId flag
+        boolean generateDeviceId = request.getAttributeBool(AccountConstants.A_GENERATE_DEVICE_ID, false);
+        newRequest.addAttribute(AccountConstants.A_GENERATE_DEVICE_ID, generateDeviceId);
+
+        // Copy deviceId
+        String deviceId = request.getAttribute(AccountConstants.E_DEVICE_ID, null);
+        if (deviceId != null) {
+            newRequest.addAttribute(AccountConstants.E_DEVICE_ID, deviceId);
+        }
+
+        // Copy optional authToken / jwtToken
+        Element authTokenEl = request.getOptionalElement(AccountConstants.E_AUTH_TOKEN);
+        if (authTokenEl != null) {
+            newRequest.addElement(AccountConstants.E_AUTH_TOKEN).setText(authTokenEl.getText());
+        }
+        Element jwtTokenEl = request.getOptionalElement(AccountConstants.E_JWT_TOKEN);
+        if (jwtTokenEl != null) {
+            newRequest.addElement(AccountConstants.E_JWT_TOKEN).setText(jwtTokenEl.getText());
+        }
+
+        // Copy optional PREAUTH
+        Element preAuthEl = request.getOptionalElement(AccountConstants.E_PREAUTH);
+        if (preAuthEl != null) {
+            Element newPreAuth = newRequest.addElement(AccountConstants.E_PREAUTH)
+                                          .setText(preAuthEl.getText());
+            newPreAuth.addAttribute(AccountConstants.A_TIMESTAMP, preAuthEl.getAttributeLong(AccountConstants.A_TIMESTAMP, 0));
+            newPreAuth.addAttribute(AccountConstants.A_EXPIRES, preAuthEl.getAttributeLong(AccountConstants.A_EXPIRES, 0));
+        }
+
+        // Copy PREFS / ATTRS / REQUESTED_SKIN elements if present
+        Element prefsEl = request.getOptionalElement(AccountConstants.E_PREFS);
+        if (prefsEl != null) {
+            newRequest.addElement(AccountConstants.E_PREFS);
+        }
+        Element attrsEl = request.getOptionalElement(AccountConstants.E_ATTRS);
+        if (attrsEl != null) {
+            newRequest.addElement(AccountConstants.E_ATTRS);
+        }
+        Element requestedSkinEl = request.getOptionalElement(AccountConstants.E_REQUESTED_SKIN);
+        if (requestedSkinEl != null) {
+            newRequest.addElement(AccountConstants.E_REQUESTED_SKIN).setText(requestedSkinEl.getText());
+        }
+
+        // Delegate to Auth
+        return super.handle(newRequest, context);
+    }
 }
